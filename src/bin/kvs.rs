c44dda9f5fb57412f93733fa3c4ab7353db8b82f
mateiidavid <madavid@expediagroup.com>
2020-09-17 21:37:47 +0100

    Add cli args using clap
    Signed-off-by: mateiidavid <madavid@expediagroup.com>

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,5 +1,42 @@
-
-
+use clap::{App, Arg};
 fn main() {
-  println!("Hello, world!");
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+                                  .version(env!("CARGO_PKG_VERSION"))
+                                  .author("Matei David <matei.david.35@gmail.com>")
+                                  .about("\nDistributed in-memory key value data store written in Rust!\nPart of my #100DaysOfCode challenge")
+                                  .subcommand(
+                                    App::new("get")
+                                            .about("Get the string value of given key")
+                                            .arg(Arg::with_name("KEY").required(true)),
+                                  )
+                                  .subcommand(
+                                          App::new("set")
+                                          .about("Set a key to a value")
+                                          .arg(Arg::with_name("KEY").required(true))
+                                          .arg(Arg::with_name("VALUE").required(true)),
+                                  )
+                                  .subcommand(
+                                        App::new("rm")
+                                        .about("Remove a given key")
+                                        .arg(Arg::with_name("KEY").required(true))
+                                  )
+                                  .get_matches();
+
+    match matches.subcommand_name() {
+        Some("get") => {
+            eprintln!("unimplemented");
+            std::process::exit(1);
+        }
+        Some("set") => {
+            eprintln!("unimplemented");
+            std::process::exit(1);
+        }
+        Some("rm") => {
+            eprintln!("unimplemented");
+            std::process::exit(1);
+        }
+        _ => {
+          std::process::exit(1);
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+pub struct KvStore {}
+
+impl KvStore {
+    pub fn new() -> Self {
+        KvStore {}
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        panic!()
+    }
+
+    pub fn set(&self, key: String, value: String) {
+        panic!()
+    }
+
+    pub fn remove(&self, key: String) {
+        panic!()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
