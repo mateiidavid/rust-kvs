056fa2202524adf6a9afa978e217e3ed91f38db9
mateiidavid <madavid@expediagroup.com>
2020-09-18 14:33:56 +0100

    Ran CLIPPY
    Signed-off-by: mateiidavid <madavid@expediagroup.com>

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,26 +1,26 @@
 use clap::{App, Arg};
 fn main() {
     let matches = App::new(env!("CARGO_PKG_NAME"))
-                                  .version(env!("CARGO_PKG_VERSION"))
-                                  .author(env!("CARGO_PKG_AUTHORS"))
-                                  .about(env!("CARGO_PKG_DESCRIPTION"))
-                                  .subcommand(
-                                    App::new("get")
-                                            .about("Get the string value of given key")
-                                            .arg(Arg::with_name("KEY").required(true)),
-                                  )
-                                  .subcommand(
-                                          App::new("set")
-                                          .about("Set a key to a value")
-                                          .arg(Arg::with_name("KEY").required(true))
-                                          .arg(Arg::with_name("VALUE").required(true)),
-                                  )
-                                  .subcommand(
-                                        App::new("rm")
-                                        .about("Remove a given key")
-                                        .arg(Arg::with_name("KEY").required(true))
-                                  )
-                                  .get_matches();
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .subcommand(
+            App::new("get")
+                .about("Get the string value of given key")
+                .arg(Arg::with_name("KEY").required(true)),
+        )
+        .subcommand(
+            App::new("set")
+                .about("Set a key to a value")
+                .arg(Arg::with_name("KEY").required(true))
+                .arg(Arg::with_name("VALUE").required(true)),
+        )
+        .subcommand(
+            App::new("rm")
+                .about("Remove a given key")
+                .arg(Arg::with_name("KEY").required(true)),
+        )
+        .get_matches();
 
     match matches.subcommand_name() {
         Some("get") => {
@@ -36,7 +36,7 @@ fn main() {
             std::process::exit(1);
         }
         _ => {
-          std::process::exit(1);
+            std::process::exit(1);
         }
     }
 }
